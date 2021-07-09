47ae0e1261923bff9621cc7bae94bb821809fa9c
100B <toannguyenas@gmail.com>
2021-07-09 18:43:58 +0700

    btvn buoi 7 - tinh tuoi

--- App.tsx
+++ App.tsx
@@ -2,35 +2,22 @@ import React from 'react';
 import { SafeAreaView, StyleSheet, TextInput, Text } from 'react-native';
 
 const UselessTextInput = () => {
-  const [Edge1, setEdge1] = React.useState("Edge1");
-  const [Edge2, setEdge2] = React.useState("Edge2");
-  const [Edge3, setEdge3] = React.useState("Edge3");
-  
-  console.log(Edge3)
-
-  function calculatePerimeter (Edge1:number, Edge2:number, Edge3:number) {
-    return ({Edge1} + {Edge2} + {Edge3})
+   const [Year, setYear] = React.useState("Year")
+  const year1:number = (2021 - +Year);
+  function calculateAge (Year:number) {
+    return ({Year})
   }
 
   return (
     <SafeAreaView>
       <TextInput
         style={styles.input}
-        onChangeText={setEdge1}
-        value={Edge1}
+        onChangeText={setYear}
+        value={Year}
       />
-      <TextInput
-        style={styles.input}
-        onChangeText={setEdge2}
-        value={Edge2}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={setEdge3}
-        value={Edge3}
-      />
+      
       <Text>
-        Chu vi: {+Edge1 + +Edge2 + +Edge3}
+        Tuoi cua ban la: ({year1})
         </Text>
     </SafeAreaView>
   );
