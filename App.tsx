b3802e350b97ba1775af36ad98914737f27beadc
Rindaaaa <nmd0909hp@gmail.com>
2021-07-09 15:36:33 +0700

    hw7

--- App.tsx
+++ App.tsx
@@ -9,40 +9,27 @@ import {
 } from 'react-native';
 
 const App = () => {
-  const [edge1, setEdge1] = useState('');
-  const [edge2, setEdge2] = useState('');
-  const [edge3, setEdge3] = useState('');
-  const [perimeter, setPerimeter] = useState(0);
+  const [yearOfBirt, setYear] = useState('');
+  const [age, setAge] = useState(0);
 
-  function calculate() {
-    const total = +edge1 + +edge2 + +edge3;
-    setPerimeter(total);
+  function calulateAge() {
+    let today = new Date();
+    const subtraction = today.getFullYear() - +yearOfBirt;
+    setAge(subtraction);
   }
 
   return (
     <SafeAreaView style={{backgroundColor: 'white', flex: 1}}>
       <View style={{margin: 12}}>
-        <Text>Canh 1</Text>
+        <Text>Nam sinh</Text>
         <TextInput
           style={styles.input}
           keyboardType="numeric"
-          onChangeText={setEdge1}
+          onChangeText={setYear}
         />
-        <Text>Canh 2</Text>
-        <TextInput
-          style={styles.input}
-          keyboardType="numeric"
-          onChangeText={setEdge2}
-        />
-        <Text>Canh 3</Text>
-        <TextInput
-          style={styles.input}
-          keyboardType="numeric"
-          onChangeText={setEdge3}
-        />
-        <Button title={'Tính'} onPress={calculate} />
+        <Button title={'Tính'} onPress={calulateAge} />
         <Text style={{textAlign: 'center', fontSize: 20}}>
-          Chu vi tam giac la: {perimeter}
+          Tuoi cua ban la: {age}
         </Text>
       </View>
     </SafeAreaView>
