50b10ff58a4f1beddd98fb55e1f4e182cff2e72d
Rindaaaa <nmd0909hp@gmail.com>
2021-07-12 20:13:47 +0700

    fix

--- App.tsx
+++ App.tsx
@@ -1,46 +1,132 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  Alert,
+  FlatList,
+  Image,
   SafeAreaView,
-  Text,
-  View,
-  TextInput,
   StyleSheet,
-  Button,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
 } from 'react-native';
+import _ from 'lodash';
+
+const productData = [
+  {
+    title: 'Lenovo Chromebook',
+    price: 279.0,
+    img: 'https://anphat.com.vn/media/product/250_38438_techzones_lenovo_yoga_slim_7_14itl05_i5_1135g7_16gb_512gb_ssd_7.jpg',
+  },
+  {
+    title: 'HP Gaming Laptop',
+    price: 1400.0,
+    img: 'https://anphat.com.vn/media/product/37327_33742_hp_pavilion_gaming_15_ec1054ax_1n1h6pa_r5_4600h_542013861d504ab0a9822ffab255fe17_master.png',
+  },
+  {
+    title: 'HP EliteOne',
+    price: 1500.0,
+    img: 'https://anphat.com.vn/media/product/36581_hp_omen_15_ek0078tx_26y68pa_i7_10750h_2_ee34bb88dd1d4e22b3ebc6ea5002a648_master.png',
+  },
+  {
+    title: 'HP Pavilion Gaming',
+    price: 1200.0,
+    img: 'https://anphat.com.vn/media/product/37087_18785_laptop_hp_pavilion_15_eg0073tu_2p1n4pa_1.png',
+  },
+  {
+    title: 'AKKO 3096 ',
+    price: 1200.0,
+    img: 'https://anphat.com.vn/media/product/34372_akko_3096_world_tour_tokyo_r2.jpg',
+  },
+  {
+    title: 'AKKO 3108 V2 Bilibili',
+    price: 1300.0,
+    img: 'https://anphat.com.vn/media/product/34705_akko_3108_v2_bilibili__1_.jpg',
+  },
+];
+
+type CustomItem = {
+  title: string;
+  price: number;
+  img: string;
+};
+
+const Item = ({item}: {item: CustomItem}) => (
+  <TouchableOpacity
+    style={styles.itemBox}
+    onPress={() =>
+      Alert.alert(
+        'Giá của sản phẩm: $ ' + item.price + '\nTên của sản phẩm: ' + item.title,
+      )
+    }>
+    <Image style={styles.image} source={{uri: item.img}} />
+    <Text style={{marginTop: 30}}>{item.title}</Text>
+    <Text style={{marginTop: 30, color: 'red'}}>${item.price}</Text>
+  </TouchableOpacity>
+);
 
 const App = () => {
-  const [yearOfBirt, setYear] = useState('');
-  const [age, setAge] = useState(0);
+  const [filterData, setFilterData] = useState(productData);
 
-  function calulateAge() {
-    let today = new Date();
-    const subtraction = today.getFullYear() - +yearOfBirt;
-    setAge(subtraction);
+  function searchProduct(textToSearch: string){console.log(textToSearch);
+    setFilterData(productData.filter(product => product.title.includes(textToSearch)))
   }
 
+  const renderItem = ({item}: {item: CustomItem}) => <Item item={item} />;
+
+  useEffect(()=>{
+    const interval = setInterval(()  =>{
+      setFilterData(_.shuffle(filterData))
+    }, 5000)
+    return () => {
+      clearInterval(interval);
+    };
+  },[filterData.length, setFilterData])
+
   return (
-    <SafeAreaView style={{backgroundColor: 'white', flex: 1}}>
-      <View style={{margin: 12}}>
-        <Text>Nam sinh</Text>
+    <SafeAreaView style={{backgroundColor: 'white'}}>
+      <View>
         <TextInput
-          style={styles.input}
-          keyboardType="numeric"
-          onChangeText={setYear}
+          onChangeText={(text) => searchProduct(text) }       
+          style={styles.search}
+          placeholder={' Tìm kiếm'}
         />
-        <Button title={'Tính'} onPress={calulateAge} />
-        <Text style={{textAlign: 'center', fontSize: 20}}>
-          Tuoi cua ban la: {age}
-        </Text>
       </View>
+      <FlatList
+        numColumns={2}
+        data={filterData}
+        renderItem={renderItem}
+        keyExtractor={item => item.toString()}
+      />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
+  container: {
+    flex: 1,
+  },
+  itemBox: {
+    backgroundColor: 'white',
+    padding: 15,
+    width: 170,
+    height: 300,
+    alignItems: 'baseline',
+    borderWidth: 0.5,
+    margin: 10,
+    borderRadius: 10,
+  },
+  image: {
+    width: '100%',
+    height: 120,
+  },
+  search: {
+    backgroundColor: 'white',
+    margin: 10,
     borderWidth: 1,
+    borderRadius: 10,
+    borderColor: 'black',
   },
 });
+
 export default App;
